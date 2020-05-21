c40c94fe2521a6495ec7da281a794e91ec41c3fc
Erjanobk <54842730+Erjanobk@users.noreply.github.com>
2020-05-21 13:03:08 +0200

    Erjano

---
+++ Chapeau_Group9/ChapeauUI/ChapeauLogic/MenuItemServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChapeauDAL;
+using ChapeauModel;
+
+namespace ChapeauLogic
+{
+    public class MenuItemServices
+    {
+        MenuItemDAO menuItem = new MenuItemDAO();
+        public List<MenuItem> GetMenuItems()
+        {
+            try
+            {
+                List<MenuItem> items = new List<MenuItem>();
+                items = menuItem.GetAllMenuItems();
+                return items;
+            }
+            catch (Exception)
+            {
+                List<MenuItem> menuItems = new List<MenuItem>();
+                MenuItem item1 = new MenuItem();
+                item1.MenuItemID = 00000;
+                item1.Name = "Empty";
+                item1.Price = 00000;
+                item1.Stock = 11111;
+                item1.VAT = false;
+                item1.Lunch = false;
+                item1.Category = CategoryID.empty;
+
+                menuItems.Add(item1);
+                return menuItems;
+                throw new Exception("Couldn't connect to the database");
+
+            }
+        }
+    }
+}
