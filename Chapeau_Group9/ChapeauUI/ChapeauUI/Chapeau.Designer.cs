220e0329457d9894a4d234df39d76345a50cb98b
elias19119 <eleaghanem8@gmail.com>
2020-05-23 22:50:41 +0200

    Chapeau Login Update

--- Chapeau_Group9/ChapeauUI/ChapeauUI/Chapeau.Designer.cs
+++ Chapeau_Group9/ChapeauUI/ChapeauUI/Chapeau.Designer.cs
@@ -30,7 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Chapeau));
             this.loginbtn = new System.Windows.Forms.Button();
-            this.lblloginbox = new System.Windows.Forms.TextBox();
+            this.logintextbox = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -41,32 +41,29 @@
             this.loginbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.loginbtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.loginbtn.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.loginbtn.Location = new System.Drawing.Point(215, 330);
-            this.loginbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loginbtn.Location = new System.Drawing.Point(161, 268);
             this.loginbtn.Name = "loginbtn";
-            this.loginbtn.Size = new System.Drawing.Size(216, 55);
+            this.loginbtn.Size = new System.Drawing.Size(162, 45);
             this.loginbtn.TabIndex = 0;
             this.loginbtn.Text = "Login";
             this.loginbtn.UseVisualStyleBackColor = false;
             this.loginbtn.Click += new System.EventHandler(this.lbllogin_Click);
             // 
-            // lblloginbox
+            // logintextbox
             // 
-            this.lblloginbox.Location = new System.Drawing.Point(215, 235);
-            this.lblloginbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.lblloginbox.Name = "lblloginbox";
-            this.lblloginbox.Size = new System.Drawing.Size(215, 22);
-            this.lblloginbox.TabIndex = 1;
-            this.lblloginbox.TextChanged += new System.EventHandler(this.lblloginbox_TextChanged);
+            this.logintextbox.Location = new System.Drawing.Point(161, 191);
+            this.logintextbox.Name = "logintextbox";
+            this.logintextbox.Size = new System.Drawing.Size(162, 20);
+            this.logintextbox.TabIndex = 1;
+            this.logintextbox.TextChanged += new System.EventHandler(this.lblloginbox_TextChanged);
             // 
             // pictureBox1
             // 
             this.pictureBox1.ErrorImage = null;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(215, 2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(161, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(216, 110);
+            this.pictureBox1.Size = new System.Drawing.Size(162, 89);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -74,14 +71,13 @@
             // Chapeau
             // 
             this.AcceptButton = this.loginbtn;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.ClientSize = new System.Drawing.Size(624, 468);
+            this.ClientSize = new System.Drawing.Size(468, 380);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.lblloginbox);
+            this.Controls.Add(this.logintextbox);
             this.Controls.Add(this.loginbtn);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Chapeau";
@@ -97,7 +93,7 @@
 
         private System.Windows.Forms.Button loginbtn;
         private System.Windows.Forms.PictureBox pictureBox1;
-        public System.Windows.Forms.TextBox lblloginbox;
+        public System.Windows.Forms.TextBox logintextbox;
     }
 }
 
--- Chapeau_Group9/ChapeauUI/ChapeauUI/TablesOverview.cs
+++ Chapeau_Group9/ChapeauUI/ChapeauUI/TablesOverview.cs
@@ -9,15 +9,19 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using ChapeauModel;
 using System.Data.SqlClient;
+using ChapeauLogic;
 
 
 namespace ChapeauUI
 {
     public partial class TablesOverview : Form
     {
+        private List<Employee> employees;
         public TablesOverview()
         {
             InitializeComponent();
+            EmployeeServices employeeServices = new EmployeeServices();
+            employees = employeeServices.GetEmployees();
         }
         private void lbllogoutbtn_Click(object sender, EventArgs e)
         {
@@ -25,5 +29,18 @@ namespace ChapeauUI
             new Chapeau().ShowDialog();
         }
 
+        private void infobtn_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnUnpay_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
+
+
+
+
