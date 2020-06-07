e0b334e1f3691144cd59dc9ac1db5c721d039bac
Erjanobk <54842730+Erjanobk@users.noreply.github.com>
2020-06-07 16:44:51 +0200

    erjano

--- Chapeau_Group9/ChapeauUI/ChapeauUI/Chapeau.Designer.cs
+++ Chapeau_Group9/ChapeauUI/ChapeauUI/Chapeau.Designer.cs
@@ -82,6 +82,7 @@
             this.MinimizeBox = false;
             this.Name = "Chapeau";
             this.Text = "Chapeau";
+            this.Load += new System.EventHandler(this.Chapeau_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
