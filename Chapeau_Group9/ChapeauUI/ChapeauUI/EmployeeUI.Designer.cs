f1db2b1b2aef7c23fdad271c6362187cdd6ef076
Erjanobk <54842730+Erjanobk@users.noreply.github.com>
2020-06-29 20:51:50 +0200

    er

--- Chapeau_Group9/ChapeauUI/ChapeauUI/EmployeeUI.Designer.cs
+++ Chapeau_Group9/ChapeauUI/ChapeauUI/EmployeeUI.Designer.cs
@@ -236,9 +236,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(565, 143);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.Size = new System.Drawing.Size(106, 25);
             this.label2.TabIndex = 18;
-            this.label2.Text = "2 = Waitor";
+            this.label2.Text = "2 = Server";
             // 
             // label1
             // 
