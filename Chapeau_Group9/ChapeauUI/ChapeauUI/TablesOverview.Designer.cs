5687494896e469c3edb5e9618fe92e75da00c5f7
Erjanobk <54842730+Erjanobk@users.noreply.github.com>
2020-06-30 12:21:53 +0200

    er

--- Chapeau_Group9/ChapeauUI/ChapeauUI/TablesOverview.Designer.cs
+++ Chapeau_Group9/ChapeauUI/ChapeauUI/TablesOverview.Designer.cs
@@ -569,7 +569,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1008, 721);
+            this.ClientSize = new System.Drawing.Size(1008, 729);
             this.Controls.Add(this.btnManagerUI);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnRefresh);
